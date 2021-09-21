e4afd85a08965c2f3225995b0fe029befdaf32d6
DayanandGhelaro <dayanandghelaro@A006-00592.arbisoft.com>
2021-09-21 12:34:24 +0500

    feat: install react-icons and add Task component

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,10 +1,12 @@
+import Task from "./Task"
 
 const Tasks = ({ tasks }) => {
 
     return (
         <>
             {tasks.map((task) =>(
-            <h3 key={task.id}>{task.text}</h3>
+            // <h3 key={task.id}>{task.text}</h3>
+            <Task key={task.id} task={task}/>
             ))}
         </>
     )
