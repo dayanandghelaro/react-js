26e000b3bcd6dab8ce928ca259bc9bd746dc9f62
DayanandGhelaro <dayanandghelaro@A006-00592.arbisoft.com>
2021-09-21 15:33:46 +0500

    feat: add new data to server using API

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -21,21 +21,35 @@ function App() {
 
   // Fetch tasks
   const fetchTasks = async () => {
-    const result = await fetch("http://localhost:5000/tasks")
-    const data = await result.json()
+    const response = await fetch("http://localhost:5000/tasks")
+    const data = await response.json()
     return data
   }
 
 
   // ADD Task
-  const addTask = (task)=>{
-    const newTask = {"id": Math.floor(Math.random()* 10000)+1, ...task}
+  const addTask = async (task)=>{
+ 
+    const response = await fetch("http://localhost:5000/tasks/",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(task)
+    })
+
+    const newTask = await response.json()
     setTasks([...tasks, newTask])
   }
 
 
   // DELETE a TASK
-  const deleteTask = (id)=>{
+  const deleteTask = async (id)=>{
+
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:"DELETE"
+    })
+
     setTasks(tasks.filter(task => task.id !== id))
   }
 
