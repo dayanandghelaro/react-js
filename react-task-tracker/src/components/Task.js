a9f1b42eaed64c5ecfb6ab1f9c79ff991a85a777
DayanandGhelaro <dayanandghelaro@A006-00592.arbisoft.com>
2021-09-21 12:47:58 +0500

    feat: add deleteTask functionality

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,8 +1,16 @@
+import { FaTimes } from 'react-icons/fa'
 
-const Task = ({task}) => {
+
+const Task = ({task, onDelete}) => {
     return (
         <div className="task">
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} 
+                <FaTimes 
+                    style={{ color:'red', cursor:"pointer", }}
+                    onClick = {() => {onDelete(task.id)}}
+                />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,12 +1,12 @@
 import Task from "./Task"
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
 
     return (
         <>
             {tasks.map((task) =>(
             // <h3 key={task.id}>{task.text}</h3>
-            <Task key={task.id} task={task}/>
+            <Task key={task.id} task={task} onDelete={onDelete} />
             ))}
         </>
     )
