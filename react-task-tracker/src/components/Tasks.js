b92bf5adbaec8b49263d2f2bc1bd735510f22a6a
DayanandGhelaro <dayanandghelaro@A006-00592.arbisoft.com>
2021-09-21 16:55:15 +0500

    fix: removed unnecessary code

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -5,7 +5,6 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task) =>(
-            // <h3 key={task.id}>{task.text}</h3>
             <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}  />
             ))}
         </>
